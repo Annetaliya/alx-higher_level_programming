efd2bc23a60ea95d0fba08b09789645e6a1deb4b
Annette Adhiambo <annetaliya@gmail.com>
2022-11-30 04:56:14 -0800

    sort insert

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  *insert_node - function that inserts a node to
@@ -13,21 +14,22 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *temp = *head;
 	listint_t *new_node;
 
-	new_node = malloc(sizeof(listin_t));
+	new_node = malloc(sizeof(listint_t));
 	new_node->n = number;
 	new_node->next = NULL;
 
-	int key = number;
 
-	if (head == NULL || key < *head->n)
+	if (*head == NULL || (*head)->n >= new_node->n)
 	{
 		new_node->next = *head;
 		*head = new_node;
 	}
 	else
 	{
-		while (temp->next != NULL && temp->next->n < key)
+		while (temp->next != NULL && temp->next->n < new_node->n)
+		{
 			temp = temp->next;
+		}
 		new_node->next = temp->next;
 		temp->next = new_node;
 	}
