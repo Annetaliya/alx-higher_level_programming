8683d6cf071d4a9dd4af76f71db2377ae6182cae
Annette Adhiambo <annetaliya@gmail.com>
2022-12-07 12:01:38 -0800

    max

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None
+        return (None)
+    for key, value in a_dictionary.item():
+        max = a_dictionary[0]
+        if a_dictionary[key] > max:
+            max = a_dictionary[key]
+    return (max)
