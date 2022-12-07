eeb2649cf6d05cb0f9f7de0244b2c128a4d20a4b
Annette Adhiambo <annetaliya@gmail.com>
2022-12-07 10:27:53 -0800

    update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     for key, value in (sorted(a_dictionary.items())):
-        a_dictionary[key] = value
+        if a_dictionary == key:
+            a_dictionary = value
+        else:
+            a_dictionary.update({})
     return (a_dictionary)
