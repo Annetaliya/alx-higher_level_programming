54641dcda475dc9a932ba12604e3eb985e30c427
Annette Adhiambo <annetaliya@gmail.com>
2022-12-20 07:34:27 -0800

    attributye

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Class square with size attributes """
+
+
+class Square:
+    """creates a private attributes for class square """
+
+    def __init__(self, size):
+        """ size represent the size of square defined """
+
+        self.__size = size
