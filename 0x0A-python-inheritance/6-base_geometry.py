c66ff452edbe9f48673c9e8b02adaa99fc0f19a2
Annette Adhiambo <annetaliya@gmail.com>
2023-01-09 06:17:14 -0800

    class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Creating an empty class"""
+
+
+class BaseGeometry:
+    """class with area calc method"""
+
+    def area(self):
+        """Method taht calc the ares"""
+
+        raise Exception("area() is not implemented")
