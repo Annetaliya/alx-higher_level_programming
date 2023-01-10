2fd277fda7c3beffbfae81c60c78e6e907ddad7f
Annette Adhiambo <annetaliya@gmail.com>
2023-01-10 07:18:51 -0800

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
     """prints content of a utf8 text file"""
 
-    with open('my_file_0.txt', 'r', encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
