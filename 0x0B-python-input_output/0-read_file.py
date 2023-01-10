f45ac5c8512adb16a4a4b68e987582f8cf0a94ee
Annette Adhiambo <annetaliya@gmail.com>
2023-01-10 07:11:49 -0800

    read

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that raeds a file """
+
+
+def read_file(filename=""):
+    with open('my_file_0.txt', 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+#Solution to python tasks on file input and output
