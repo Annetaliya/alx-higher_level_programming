e2a044fbb9425ac3508214b2acdb321fea8046a1
Annette Adhiambo <annetaliya@gmail.com>
2023-01-13 22:30:29 -0800

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""Declaration of a class """
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """A class that inherits attributes of parent base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """sets width to an integer value"""
+        if (type(value) is not int):
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """sets height to an int value"""
+        if (type(value) is not int):
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """sets x to a value"""
+        if (type(value) is not int):
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """sets y to a value"""
+        if (type(value) is not int):
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
+        self.__y = value
+
+    def area(self):
+        """calculates the area of a rectangle"""
+        return (self.__width * self.__height)
