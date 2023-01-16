f3bcfd53e0dccb488cf168a9d9999f034dd181a6
Annette Adhiambo <annetaliya@gmail.com>
2023-01-16 00:21:45 -0800

    dict

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -57,3 +57,10 @@ class Square(Rectangle):
                     self.x = v
                 if k == "y":
                     self.y = v
+
+    def to_dictionary(self):
+        """returns a dict rep of a Square"""
+        sq_dic = {'id': self.id, 'size': self.size,
+                  'x': self.x, 'y': self.y}
+
+        return sq_dic
