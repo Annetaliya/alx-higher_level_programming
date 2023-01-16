71327e6bfb44aa9752393a4547b8286bd1a60c9d
Annette Adhiambo <annetaliya@gmail.com>
2023-01-15 23:55:26 -0800

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,3 +30,30 @@ class Square(Rectangle):
             raise ValueError('width must be > 0')
         self.__width = value
         self.__height = value
+
+    def update(self, *args, **kwargs):
+        """assigning of attributes"""
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an integer")
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+
+        else:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if type(v) != int and v is not None:
+                        raise TypeError('id must be an integer')
+                    self.id = v
+                if k == "size":
+                    self.size = v
+                if k == "x":
+                    self.x = v
+                if k == "y":
+                    self.y = v
