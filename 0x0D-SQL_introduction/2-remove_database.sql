e07431eab6b6830c3ce13ca7472ac51915b6d0fd
Annette Adhiambo <annetaliya@gmail.com>
2023-02-14 10:12:47 -0800

    drop

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,5 +1,2 @@
 --deletes the database hbtn_0c_0 in your MySQL server.
---If the database hbtn_0c_0 doesn’t exist, your script should not fail
--- You are not allowed to use the SELECT or SHOW statements
-
 DROP DATABASE EXIST hbtn_0c_0;
