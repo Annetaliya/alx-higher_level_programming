fe1aa52090fd9ca2df9a6ae4c3dd0a7fc8b5fc46
Annette Adhiambo <annetaliya@gmail.com>
2023-02-15 10:00:02 -0800

    primary key

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- a script that creates the database hbtn_0d_usa and the table states
+-- id INT unique, auto generated, can’t be null and is a primary key
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	id INT UNIQUE PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL);
+
