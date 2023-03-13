6dd6bf8783b69472d65f5e916298a04d50cc9ae2
Annette Adhiambo <annetaliya@gmail.com>
2023-03-13 05:15:52 -0700

    argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const process = require('process');
-if (process === 0) {
+const argc = process.argv.length;
+if (argc === 0) {
   console.log('No argement');
-} else if (process === 1) {
+} else if (argc === 1) {
   console.log('Argument found');
 } else { console.log('Arguments found'); }
