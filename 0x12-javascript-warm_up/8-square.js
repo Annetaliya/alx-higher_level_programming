ff6a0ddd123d84a158af21af002fd0b70b872872
Annette Adhiambo <annetaliya@gmail.com>
2023-03-13 08:48:08 -0700

    nested if

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const square = parseInt(process.argv[2]);
+if (isNaN(square)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < square; i++) {
+    let row = '';
+    for (let j = 0; j < square; j++) row += 'X';
+    console.log(row);
+  }
+}
