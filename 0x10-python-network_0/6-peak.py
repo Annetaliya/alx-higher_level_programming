aa908b49ec7587da1121979ea78332e7788dce0a
Annette Adhiambo <annetaliya@gmail.com>
2023-03-30 12:18:41 -0700

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,21 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+'''Returns a peak in a list '''
+
 
 def find_peak(list_of_integers):
-    max = 0
-    for i in list_of_integers:
-        if i > max:
-            max = i
-        elif list_of_integers == ():
-            return None
-    return max
+    if not list_of_integers:
+        return None
+
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+
+    if list_of_integers[0] >= list_of_integers[1]:
+        return list_of_integers[0]
+    if list_of_integers[-1] >= list_of_integers[-2]:
+        return list_of_integers[-1]
+
+    for i in range(1, len(list_of_integers) - 1):
+        if list_of_integers[i] >= list_of_integers[i - 1] and list_of_integers[i] >= list_of_integers[i + 1]:
+            return list_of_integers[i]
+
+    return None
